383f93bda7389d169ba8818a99792494ad4d9086
Veselina Zatchepina <techpro20@mail.ru>
2017-11-19 21:16:47 +0300

    feat: add CreateUserActivity

---
+++ app/src/main/java/com/github/veselinazatchepina/smack/CreateUserActivity.kt
@@ -0,0 +1,28 @@
+package com.github.veselinazatchepina.smack
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun createUserClicked(view: View) {
+
+    }
+
+    fun generateColorClicked(view: View) {
+
+    }
+
+
+
+}
